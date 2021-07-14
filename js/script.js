d4dbe1af4ccf28676830fbc33e1a7b91912da80b
ansalf <anissaalifia6@gmail.com>
2021-07-14 12:47:46 +0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let menu = document.querySelector('#menu-bars');
+let header = document.querySelector('header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+let cursor1 = document.querySelector('.cursor-1');
+let cursor2 = document.querySelector('.cursor-2');
+
+window.onmousemove = (e) =>{
+}
+
+document.querySelectorAll('a').forEach(links =>{
+
+    links.onmouseenter = () =>{
+
+        cursor2.classList.add('active');
+    }
+
+    links.onmouseleave = () =>{
+
+        cursor2.classList.remove('active');
+    }
+
+});
